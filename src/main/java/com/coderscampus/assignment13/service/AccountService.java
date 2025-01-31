926cf8cd7a01d6d1c21eb4c70068fd2792b1671b
Casey Michael <70309758+OmnibusAnalysis@users.noreply.github.com>
2025-01-30 19:21:37 -0800

    update

--- src/main/java/com/coderscampus/assignment13/service/AccountService.java
+++ src/main/java/com/coderscampus/assignment13/service/AccountService.java
@@ -1,9 +1,12 @@
 package com.coderscampus.assignment13.service;
 
 import com.coderscampus.assignment13.domain.Account;
+import com.coderscampus.assignment13.domain.User;
 import com.coderscampus.assignment13.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AccountService {
@@ -15,6 +18,8 @@ public class AccountService {
         return accountRepo.save(account);
     }
 
-    public Account findById(Long accountId) {
+    public Account findById(Long userId) {
+        Optional<Account> userOpt = accountRepo.findById(userId);
+        return userOpt.orElse(new Account());
     }
 }
